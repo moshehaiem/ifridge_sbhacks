9803313b955ab55e02fc23cc9e81ff3ce3d8b077
Moshe Haiem <mhaiem9@gmail.com>
2020-01-11 12:12:54 -0800

    test_commit_2

---
+++ ifridge_sbhacks/iFridge/iFridge/FirstViewController.swift
@@ -0,0 +1,92 @@
+//
+//  FirstViewController.swift
+//  iFridge
+//
+//  Created by Patrick Kuang on 1/11/20.
+//  Copyright © 2020 Patrick Kuang. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var dateField: UITextField!
+    
+    let picker = UIDatePicker()
+    
+    var groceryList = ["eggs", "chicken", "milk"]
+    
+    @IBOutlet weak var myTableView: UITableView!
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return (groceryList.count)
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "cell")
+        cell.textLabel?.text = groceryList[indexPath.row]
+        
+        return(cell)
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath)
+    {
+        if editingStyle == UITableViewCell.EditingStyle.delete
+        {
+            self.groceryList.remove(at: indexPath.row)
+            myTableView.reloadData()
+        }
+        
+    }
+    
+    @IBOutlet weak var input: UITextField!
+    
+    @IBAction func addItem(_ sender: Any)
+    {
+        if (input.text != "")
+        {
+            self.groceryList.append(input.text!)
+            input.text = ""
+            myTableView.reloadData()
+        }
+        
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        createDatePicker()
+    }
+    
+    func createDatePicker() {
+        //toolbar
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        
+        //done button for toolbar
+        let done = UIBarButtonItem(barButtonSystemItem: .done, target:nil, action: #selector(donePressed))
+        toolbar.setItems([done], animated: false)
+        
+        dateField.inputAccessoryView = toolbar
+        dateField.inputView = picker
+        
+        //format picker for date
+        picker.datePickerMode = .date
+    }
+    
+    @objc func donePressed() {
+        // format date
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .none
+        let dateString = formatter.string(from: picker.date)
+        
+        dateField.text = "\(dateString)"
+        self.view.endEditing(true)
+    }
+
+}
