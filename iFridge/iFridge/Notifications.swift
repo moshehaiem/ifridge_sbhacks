183b22bd35898014197bde343e0e9353c71b8053
Zion Wang <zion@ucsb.umail.edu>
2020-01-11 12:02:51 -0800

    Notfication base class, a notification class and a struct for a array of notifications + functions

---
+++ iFridge/iFridge/Notifications.swift
@@ -0,0 +1,78 @@
+//
+//  Notifications.swift
+//  iFridge
+//
+//  Created by Zion Wang on 1/11/20.
+//  Copyright © 2020 Patrick Kuang. All rights reserved.
+//
+
+import SwiftUI
+import UserNotifications
+
+class LocalNotificationManager
+{
+    var notifications = [Notification]()
+    
+    func listScheduledNotifications()
+    {
+        UNUserNotificationCenter.current().getPendingNotificationRequests { notifications in
+
+            for notification in notifications {
+                print(notification)
+            }
+        }
+    }
+
+    private func requestAuthorization()
+    {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { granted, error in
+
+            if granted == true && error == nil {
+                self.scheduleNotifications()
+            }
+        }
+    }
+
+    func schedule()
+    {
+        UNUserNotificationCenter.current().getNotificationSettings { settings in
+
+            switch settings.authorizationStatus {
+            case .notDetermined:
+                self.requestAuthorization()
+            case .authorized, .provisional:
+                self.scheduleNotifications()
+            default:
+                break // Do nothing
+            }
+        }
+    }
+
+    private func scheduleNotifications()
+    {
+        for notification in notifications
+        {
+            let content      = UNMutableNotificationContent()
+            content.title    = notification.title
+            content.sound    = .default
+
+            let trigger = UNCalendarNotificationTrigger(dateMatching: notification.datetime, repeats: false)
+
+            let request = UNNotificationRequest(identifier: notification.id, content: content, trigger: trigger)
+
+            UNUserNotificationCenter.current().add(request) { error in
+
+                guard error == nil else { return }
+
+                print("Notification scheduled! --- ID = \(notification.id)")
+            }
+        }
+    }
+}
+
+struct Notification {
+    var id:String
+    var title:String
+    var datetime:DateComponents
+}
+
